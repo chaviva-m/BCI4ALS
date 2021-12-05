192fc10d14b136682cac20649a9627caa07434a7
Nitai Seri <Nitai.ns@gmail.com>
2021-12-05 10:21:26 +0200

    mi5 starting

--- MI5_modelTraining.m
+++ MI5_modelTraining.m
@@ -11,30 +11,32 @@ function [test_results] = MI5_modelTraining(recordingFolder)
 %% Read the features & labels 
 
 FeaturesTrain = cell2mat(struct2cell(load(strcat(recordingFolder,'\FeaturesTrainSelected.mat'))));   % features for train set
-LabelTrain = cell2mat(struct2cell(load(strcat(recordingFolder,'\LabelTrain.mat'))));                % label vector for train set
+LabelTrain = cell2mat(struct2cell(load(strcat(recordingFolder,'\LabelTrain.mat'))))';                % label vector for train set
 
 % label vector
-LabelTest = cell2mat(struct2cell(load(strcat(recordingFolder,'\LabelTest.mat'))));      % label vector for test set
-load(strcat(recordingFolder,'\FeaturesTest.mat'));                                  % features for test set
+LabelTest = cell2mat(struct2cell(load(strcat(recordingFolder,'\LabelTest.mat'))))';      % label vector for test set
+FeaturesTest = cell2mat(struct2cell(load(strcat(recordingFolder,'\FeaturesTest.mat'))));   % features for test set
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% Split to train and validation sets %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% test data
-FeaturesTrain = [FeaturesTrain LabelTrain'];
-s = 10;
+%FeaturesTrain = [FeaturesTrain LabelTrain'];
+rounds = 10;
+P = 0.70 ;
 
-for c = 1:s
+for c = 1:rounds
     
-    [m,n] = size(FeaturesTrain) ;
-    P = 0.70 ;
-    idx = randperm(m)  ;
-    Training = FeaturesTrain(idx(1:round(P*m)),:) ; 
-    Validation = FeaturesTrain(idx(round(P*m)+1:end),:) ;
-    
-    testPrediction = classify(FeaturesTest,FeaturesTrain,LabelTrain,'linear');          % classify the test set using a linear classification object (built-in Matlab functionality)
-    W = LDA(FeaturesTrain,LabelTrain); 
+    [samples,features_num] = size(FeaturesTrain) ;
+    idx = randperm(samples)  ;
+    Training = FeaturesTrain(idx(1:round(P*samples)),:) ; 
+    Validation = FeaturesTrain(idx(round(P*samples)+1:end),:) ;
+    LabelTraining = LabelTrain(idx(1:round(P*samples)),:) ; 
+    LabelValidation = LabelTrain(idx(round(P*samples)+1:end),:) ;
+
+    %testPrediction = classify(FeaturesTest,FeaturesTrain,LabelTrain,'linear');          % classify the test set using a linear classification object (built-in Matlab functionality)
+    W = LDA(Training,LabelTrain); 
 end
                                                  % train a linear discriminant analysis weight vector (first column is the constants)
 
